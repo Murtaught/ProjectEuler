a0875b09f8bf0b28bb09d0c2c406fad7ebc7460d
Murtaught <murtaught@gmail.com>
2013-11-01 16:59:15 +0400

    Added problem 126

--- 126/main.cpp
+++ 126/main.cpp
@@ -5,37 +5,76 @@
 using namespace std;
 typedef unsigned int uint;
 
-// Amount of cubes in layer N of cuboid with dimensions (n; m; k)
 uint layer(uint n, uint m, uint k, uint N)
 {
-    // Finally derived this formula. HELL FREAKING YEAH!
-    return 4*N*N + 4*N*(n + m + k - 3) + (2*(n*m + n*k + m*k) - 4*(n + m + k) + 8);
+    return 2*(n*m + n*k + m*k) + 4*(n + m + k + N - 2)*(N - 1);
 }
 
-void user_mode()
+const uint LAYER_CEILING = 100000;
+vector<uint> layer_pop( LAYER_CEILING + 1, 0 ); // layer popularity
+
+void brute_force()
 {
-    int n, m, k;
-    cout << "Input cuboid dimensions (n ; m ; k): " << endl;
-    cout << "  n = "; cin >> n;
-    cout << "  m = "; cin >> m;
-    cout << "  k = "; cin >> k;
+    for (int n = 1; layer(n, n, n, 1) <= LAYER_CEILING; ++n)
+        for (int m = n; layer(n, m, m, 1) <= LAYER_CEILING; ++m)
+            for (int k = m; layer(n, m, k, 1) <= LAYER_CEILING; ++k)
+                for (int N = 1; layer(n, m, k, N) <= LAYER_CEILING; ++N)
+                    ++layer_pop[layer(n, m, k, N)];
+}
 
-    cout << "\nCuboid (" << n << " ; " << m << " ; " << k << "): \n";
+void dump(const char *filename, const vector<uint> &layer_pop)
+{
+    cout << "Saving layer popularity array to file \""
+         << filename << "\"... ";
 
-    for (int N = 1; N <= 7; ++N)
-        cout << "    N = " << N << ":  " << layer(n, m, k, N) << endl;
+    ofstream file(filename);
 
-    cout << endl << layer(n, m, k, 1);
-    for (int N = 2; N <= 15; ++N)
-        cout << ", " << layer(n, m, k, N);
+    file << layer_pop.size() << "\n";
+    for (size_t i = 0; i < layer_pop.size(); ++i)
+        file << layer_pop[i] << " ";
 
-    cout << endl;
+    file.flush();
+    cout << "done." << endl;
+}
+
+void load(const char *filename, vector<uint> &layer_pop)
+{
+    cout << "Loading layer popularity array from file \""
+         << filename << "\"..." << endl;
+
+    ifstream file(filename);
+
+    size_t size;
+    file >> size;
+
+    layer_pop.clear();
+    layer_pop.resize(size);
+
+    for (size_t i = 0; i < size; ++i)
+        file >> layer_pop[i];
+
+    cout << "done." << endl;
+}
+
+void research()
+{
+    cout << "Going through " << layer_pop.size() << " elements..." << endl;
+
+    for (size_t i = 0; i < layer_pop.size(); ++i)
+        if ( layer_pop[i] == 1000 )
+        {
+            cout << "C(" << i << ") = 1000" << endl;
+            break;
+        }
 }
 
 int main()
 {
-    user_mode();
+    brute_force();
+    //dump("dump.txt", layer_pop);
+
+    //load();
+    research();
 
     return 0;
 }
-
