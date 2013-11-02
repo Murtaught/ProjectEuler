462cc202adffc42455246fb68cae583854e416bf
Murtaught <murtaught@gmail.com>
2013-11-02 20:45:26 +0400

    Added comment on problem 24

--- 24/main.cpp
+++ 24/main.cpp
@@ -3,13 +3,14 @@
 #include <string>
 using namespace std;
 
-const int MILLION = 1000 * 1000 - 1;
+const int MILLION = 1000 * 1000;
 
 int main()
 {
+    // this is 1th permutation, not 0th
     string s = "0123456789";
 
-    for (int i = 0; i < MILLION; ++i)
+    for (int i = 1; i < MILLION; ++i)
         next_permutation(s.begin(), s.end());
 
     cout << MILLION << "th permutation of numbers 0..9 is "
